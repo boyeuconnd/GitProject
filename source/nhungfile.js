9faed8a2ec68e1c1f36a9d2ea438d446d01553b9
Dang Pham <thienvu453@gmail.com>
2020-03-08 18:15:23 +0700

    Sua file theo bai thuc hanh khai bao & su dung bien

--- source/nhungfile.js
+++ source/nhungfile.js
@@ -1,4 +1,32 @@
-function showMessage(){
-    alert('Xin chào!');
-}
 
+// function showMessage(){
+//     alert('Xin chào!');
+// }
+/*var i = 10
+let f = 147;
+let b = true;
+let s = 'Nam Dinh';
+document.write('i=' + i );
+document.write('<br>')
+document.write('f=' + f );
+document.write('<br>')
+document.write('b=' + b );
+document.write('<br>')
+document.write('s=' + s );
+*/
+/*let width = 650;
+let height = 89;
+let area = width * height
+document.write('Area = '+ area);*/
+a = prompt('Nhập biến a: ')
+b = prompt('Nhập biến b: ')
+let c = a - b
+if (c > 0 ){
+    alert('Số a lớn hơn số b')
+}else {
+    if (c === 0){
+        alert('Số a và b bằng nhau')
+    } else {
+        alert('Số a nhỏ hơn số b')
+    }
+}
