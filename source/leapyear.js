c710a7078c157a02479e3d02febc2a8155cd73d9
Dang Pham <thienvu453@gmail.com>
2020-03-10 09:58:54 +0700

    Them file ktra nam nhuan

---
+++ source/leapyear.js
@@ -0,0 +1,17 @@
+let year = Number(prompt('Mời nhập năm muốn kiểm tra'));
+let check = false; //Biến check kiểm tra năm nhuận cho ra giá trị true hoặc false
+
+if (year % 4 == 0){ //Kiểm tra tính chia hết cho 4
+    if (year % 100 == 0 ){//kiểm tra tính chia hết cho 100
+        if (year % 400 == 0) {// kiểm tra tính chia hết cho 400
+         check = true; //ra kqua đúng vs cả 4 điều kiện
+        }
+    } else { // ra kqua chia hết cho 4 nhưng k chia hết cho 100 và 400
+        check = true;
+    }
+};
+if (check) {
+    alert( year + " is a leap year.");
+}else{
+    alert( year + " not a leap year.");
+};
