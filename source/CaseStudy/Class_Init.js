8c1ff539065b7cd94b0efaccf09e61bf97c02fd1
Dang Pham <thienvu453@gmail.com>
2020-03-30 16:33:54 +0700

    Thêm folder file Case_study

---
+++ source/CaseStudy/Class_Init.js
@@ -0,0 +1,98 @@
+let Class_Init = function (x, y, height, width) {
+    this.x=x;
+    this.y=y;
+    this.height=height;
+    this.width=width;
+    this.speed =10;
+    this.speedBoost = function () {
+        return this.speed = 20;
+    };
+    this.getDrawX =function () {
+        return this.x - (this.width/2);
+    };
+    this.getDrawY =function () {
+        return this.y - (this.height/2);
+    };
+    this.moveUp = function () {
+        this.y -=this.speed;
+    };
+    this.moveDown = function () {
+        this.y +=this.speed;
+    };
+    this.moveLeft = function () {
+        this.x -=this.speed;
+    };
+    this.moveRight = function () {
+        this.x +=this.speed;
+    };
+    this.draw = function (direct) {
+        switch (direct) {
+            case "Up":
+                this.height = carImgUp.height;
+                this.width = carImgUp.width;
+                ctx.drawImage(carImgUp,this.getDrawX(),this.getDrawY());
+                break;
+            case "Down":
+                this.height = carImgDown.height;
+                this.width = carImgDown.width;
+                ctx.drawImage(carImgDown,this.getDrawX(),this.getDrawY());
+                break;
+            case "Left":
+                this.height = carImgLeft.height;
+                this.width = carImgLeft.width;
+                ctx.drawImage(carImgLeft,this.getDrawX(),this.getDrawY());
+                break;
+            case "Right":
+                this.height = carImgRight.height;
+                this.width = carImgRight.width;
+                ctx.drawImage(carImgRight,this.getDrawX(),this.getDrawY());
+                break;
+        }
+
+    }
+};
+let Object = function (x,y,type,respawn) {
+    this.x=x;
+    this.y=y;
+    this.type=type;
+    this.respawn = respawn;
+    this.getCenterX =function () {
+        return this.x + 25;
+    }
+    this.getCenterY =function () {
+        return this.y + 25;
+    }
+    this.draw = function () {
+        if(this.type == true){
+            ctx.drawImage(coinImg,this.x,this.y);
+        }else {
+            ctx.drawImage(rockImg,this.x,this.y);
+        }
+    }
+};
+let Sound = function (src,status) {
+    this.src = src;
+    this.status = status;
+    this.play_pause = function () {
+        var music = document.getElementById(this.src);
+        if(this.status){
+            music.pause();
+            this.status = false;
+        }else {
+            music.play();
+            this.status = true;
+        }
+
+    };
+    this.setMusic = function () {
+        let audioTab;
+        if(this.status ===true){
+            audioTab = '<audio id="'+this.src+'"src=" '+this.src +' "loop autoplay></audio>';
+
+        }else {
+            audioTab = '<audio id="'+this.src+'"src=" '+this.src +' "loop></audio>';
+        };
+        document.getElementById("mydiv").innerHTML = audioTab;
+
+    }
+};
